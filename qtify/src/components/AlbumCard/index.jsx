bff539f4e322053a7d308b9663948c4b39f3192b
Khushhali Chauhan <khushichauhan353@gmail.com>
2024-09-23 22:07:53 +0530

    tooltip added

--- qtify/src/components/AlbumCard/index.jsx
+++ qtify/src/components/AlbumCard/index.jsx
@@ -1,29 +1,36 @@
 import { Typography } from "../Typography/Typography";
+import Tooltip from "@mui/material/Tooltip";
 import s from "./AlbumCard.module.css";
 
 export const AlbumCard = ({ album, type }) => {
   return (
-    <div className={s.root}>
-      <div className={s.album}>
-        <img src={album?.image} alt="" />
-        <div className={s.albumBottom}>
-          <div className={s.chip}>
-            {type === "songs" ? (
-              <Typography
-                label={`${album?.likes} Likes`}
-                className={s.followCount}
-              />
-            ) : (
-              <Typography
-                label={`${album?.follows} Follows`}
-                className={s.followCount}
-              />
-            )}
+    <Tooltip
+      placement="top"
+      title={album?.songs?.length > 0 ? `${album.songs.length} songs` : ""}
+      arrow
+    >
+      <div className={s.root}>
+        <div className={s.album}>
+          <img src={album?.image} alt="" />
+          <div className={s.albumBottom}>
+            <div className={s.chip}>
+              {type === "songs" ? (
+                <Typography
+                  label={`${album?.likes} Likes`}
+                  className={s.followCount}
+                />
+              ) : (
+                <Typography
+                  label={`${album?.follows} Follows`}
+                  className={s.followCount}
+                />
+              )}
+            </div>
           </div>
         </div>
-      </div>
 
-      <Typography label={album?.title} className={s.bottomText} />
-    </div>
+        <Typography label={album?.title} className={s.bottomText} />
+      </div>
+    </Tooltip>
   );
 };
